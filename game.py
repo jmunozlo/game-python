a6a177d40b1dac0f8a2501c911cabe3fa2270ad5
Jose Munoz Lopez <jmunozlo.dev@gmail.com>
2022-09-19 23:50:32 -0300

    movimiento de ataque

--- game.py
+++ game.py
@@ -21,12 +21,20 @@ parado = []
 parado.append(pygame.image.load('parado1.png'))
 parado.append(pygame.image.load('parado2.png'))
 parado.append(pygame.image.load('parado3.png'))
-parado.append(pygame.image.load('parado4.png'))
-parado.append(pygame.image.load('parado5.png'))
-parado.append(pygame.image.load('parado6.png'))
+
+ataque = []
+
+ataque.append(pygame.image.load('ataque1.png'))
+ataque.append(pygame.image.load('ataque2.png'))
+ataque.append(pygame.image.load('ataque3.png'))
 
 for imagen in parado:
     imagen.set_colorkey((255, 255, 255))
+
+for imagen in ataque:
+    imagen.set_colorkey((255, 255, 255))
+
+estado = 0
 
 cuadro_actual = 0
 while not Termino:
@@ -38,6 +46,9 @@ while not Termino:
                 velocidad = -8
             if event.key == pygame.K_DOWN:
                 velocidad = 8
+            if event.key == pygame.K_SPACE and estado == 0:
+                cuadro_actual =0
+                estado = 1
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
                 velocidad = 0
@@ -46,12 +57,23 @@ while not Termino:
 
     # actualizacion
     cuadro_actual += 0.1
-    if cuadro_actual > 5:
-        cuadro_actual = 0
+    if estado == 0:
+        if cuadro_actual > 2:
+            cuadro_actual = 0
+    if estado == 1:
+        if cuadro_actual > 1:
+            cuadro_actual = 0
+            estado = 0
     pantalla.fill(azul)
     y += velocidad
-
+    if y > 500:
+        y = 500
+    if y < 0:
+        y = 0
     # Dibujos
-    pantalla.blit(parado[int(cuadro_actual)], (50, y))
+    if estado == 0:
+        pantalla.blit(parado[int(cuadro_actual)], (50, y))
+    else:
+        pantalla.blit(ataque[int(cuadro_actual)], (50, y))
     pygame.display.flip()
     reloj.tick(60)
